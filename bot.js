923d0605a8bddec3ebe10d6217a697519578dd40
GameFozerYT <48291236+GameFozerYT@users.noreply.github.com>
2019-03-30 21:24:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,14 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "+";
 client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : Texo`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : Texo ' `);
+    console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`+help`,"http://twitch.tv/Death Shop")
+client.user.setStatus("dnd")
 });
 
 
@@ -464,4 +471,4 @@ message.channel.send("`Error`:" + Julian)
   });
 
 
-client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
+client.login(process.env.BOT_TOKEN);
